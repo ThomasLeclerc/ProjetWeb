0eb1c3b5d4c88c1643a8ce82b42b5939712c5e20
Kosinus <charlelie.petracca@gmail.com>
2014-01-24 14:47:33 +0100

    affichage calendrier

--- user.php
+++ user.php
@@ -1,19 +1,32 @@
 <?php
 require_once 'include/engine.php';
 // Si non connecté
-if(!isset($_SESSION['id'])) 
-    REDIRECT('login.php');
+/*if(!isset($_SESSION['id'])) 
+    REDIRECT('login.php');*/
 HTML_HEADER('Page perso');
 ?>
-	<div id="content">
-		
-		<?php require('/var/www/ProjetWeb/agenda/selectable.php'); ?>
-			
-	</div>
 
+	<div id='calendar'></div>
 	
+	<script type="text/javascript">
+	$(document).ready(function()
+		{
+			// page is now ready, initialize the calendar..			
+			$('#calendar').fullCalendar({
+				// put your options and callbacks here
+			})
+			
+			$('#tabs').tabs({
+				show: function (event, ui) {
+					$('#calendar').fullCalendar('render');
+				}
+			});
+			
+		}
+	);
 	
-	
+
+	</script>
 	
 	
 <?php 
