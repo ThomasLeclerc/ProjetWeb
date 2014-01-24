c4dcd0e8fd3d0461e18d97ce6e7588b0fef470d3
Kosinus <charlelie.petracca@gmail.com>
2014-01-24 16:40:47 +0100

    ajout des evenements

--- user.php
+++ user.php
@@ -1,8 +1,9 @@
 <?php
+define('NO_SQL',1);
 require_once 'include/engine.php';
 // Si non connecté
-/*if(!isset($_SESSION['id'])) 
-    REDIRECT('login.php');*/
+if(!isset($_SESSION['id'])) 
+    REDIRECT('login.php');
 HTML_HEADER('Page perso');
 ?>
 
@@ -10,99 +11,21 @@ HTML_HEADER('Page perso');
 	<script type="text/javascript">
 	$(document).ready(function()
 		{
-			// page is now ready, initialize the calendar..
-	
 			$('#calendar').fullCalendar({
 				// put your options and callbacks here
+				eventSources: [
 
+					// your event source
+					{
+						url: '/events.php', // use the `url` property
+						color: 'yellow',    // an option!
+						textColor: 'black'  // an option!
+					}
+				]
 			})
-			
-			
 		}
 	);
-	/*
-	
-		$(document).ready(function() {
-	
-		var date = new Date();
-		var d = date.getDate();
-		var m = date.getMonth();
-		var y = date.getFullYear();
-		
-		var calendar = $('#calendar').fullCalendar({
-			header: {
-				left: 'prev,next today',
-				center: 'title',
-				right: 'month,agendaWeek,agendaDay'
-			},
-			selectable: true,
-			selectHelper: true,
-			select: function(start, end, allDay) {
-				var title = prompt('Event Title:');
-				if (title) {
-					calendar.fullCalendar('renderEvent',
-						{
-							title: title,
-							start: start,
-							end: end,
-							allDay: allDay
-						},
-						true // make the event "stick"
-					);
-				}
-				calendar.fullCalendar('unselect');
-			},
-			editable: true,
-			events: 'eventsJson.php'
-			/*[
-				{
-					title: 'All Day Event',
-					start: new Date(y, m, 1)
-				},
-				{
-					title: 'Long Event',
-					start: new Date(y, m, d-5),
-					end: new Date(y, m, d-2)
-				},
-				{
-					id: 999,
-					title: 'Repeating Event',
-					start: new Date(y, m, d-3, 16, 0),
-					allDay: false
-				},
-				{
-					id: 999,
-					title: 'Repeating Event',
-					start: new Date(y, m, d+4, 16, 0),
-					allDay: false
-				},
-				{
-					title: 'Meeting',
-					start: new Date(y, m, d, 10, 30),
-					allDay: false
-				},
-				{
-					title: 'Lunch',
-					start: new Date(y, m, d, 12, 0),
-					end: new Date(y, m, d, 14, 0),
-					allDay: false
-				},
-				{
-					title: 'Birthday Party',
-					start: new Date(y, m, d+1, 19, 0),
-					end: new Date(y, m, d+1, 22, 30),
-					allDay: false
-				},
-				{
-					title: 'Click for Google',
-					start: new Date(y, m, 28),
-					end: new Date(y, m, 29),
-					url: 'http://google.com/'
-				}
-			]
-		});
-		
-	});*/
+
 	</script>
 	<style>
 
