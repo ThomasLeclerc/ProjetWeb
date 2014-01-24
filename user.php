d2f4c06c2f373b0b95d5c50ea38e37160cdbc11c
Kosinus <charlelie.petracca@gmail.com>
2014-01-24 14:17:06 +0100

    creation

---
+++ user.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'include/engine.php';
+// Si non connecté
+if(!isset($_SESSION['id'])) 
+    REDIRECT('login.php');
+HTML_HEADER('Page perso');
+?>
+	<div id="content">
+		
+		<?php require('/var/www/ProjetWeb/agenda/selectable.php'); ?>
+			
+	</div>
+
+	
+	
+	
+	
+	
+<?php 
+HTML_FOOTER();
+?>
