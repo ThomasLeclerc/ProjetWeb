88c38b8833fd2503991cf0c88f85da89696ca4be
Kosinus <charlelie.petracca@gmail.com>
2014-01-27 20:36:53 +0100

    affichage info de la base sur le calendrier

--- events.php
+++ events.php
@@ -1,27 +1,20 @@
 <?php
 
 	require_once 'include/engine.php';
-/*
-	$query=SQL("	select * 
-					from EVENEMENT e, APPARTIENT a, UTILISATEUR u, GROUPE g
-					where u.loginUtilisateur='cpetracc' 
-					and a.idUtilisateur=u.idUtilisateur
-					and e.idGroupe=a.idGroupe");
-*/
-	$query=SQL("select * from EVENEMENT");
 
-	$year = date('Y');
-	$month = date('m');
+	//affiche les evenements des groupes dont fait partie l'utilisateur
+	$query=SQL("	select e.* 
+					from EVENEMENT e, APPARTIENT a
+					where a.idUtilisateur=".$_SESSION['id']."
+					and e.idGroupe=a.idGroupe" );
 
 	$events = array();
 	
-	$row=$query->fetch_object();
+	while($row=$query->fetch_object()){
 	
-	array_push($events,array('id'=> 1, 'title'=>$row->title, 'start' => "$year-$month-07"));
-	
-	$row=$query->fetch_object();
-	
-	array_push($events,array('id'=> 2, 'title'=>$row->title, 'start' => "2014-01-08"));
+		array_push($events,array('id'=> $row->id , 'title'=>$row->title, 'start' => $row->start, 'end' => $row->end));
+		
+	}
 
 	echo json_encode($events);
 	
